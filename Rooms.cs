3168ea77aaa3cdbfb6f6d37e22a65245ddcc841e
mikoreo <michael@de-ru.net>
2026-05-21 09:15:34 +0200

    started on main room

--- Rooms.cs
+++ Rooms.cs
@@ -8,5 +8,15 @@ namespace C_ConsoleGame
 {
     internal class Rooms
     {
+        private string _name;
+        private string _description;
+        private bool _solvedPuzzle;
+        private int _sanity;
+        public Rooms(string Name, string Description, int Sanity)
+        {
+            _name = Name;
+            _description = Description;
+            _sanity = Sanity;
+        }
     }
 }
