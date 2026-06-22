4c6a97b6a74e5a773c4feb2ef3c5504b72773de6
mikoreo <michael@de-ru.net>
2026-06-22 22:43:38 +0200

    Created parent class

---
+++ ParentClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ConsoleGame
+{
+    internal class ParentClass
+    {
+        public abstract class Kamer
+        {
+            public string Naam { get; set; }
+            public string Beschrijving { get; set; }
+            public bool IsOpgelost { get; protected set; } = false;
+
+            public Kamer(string naam, string beschrijving)
+            {
+                Naam = naam;
+                Beschrijving = beschrijving;
+            }
+            public virtual void StartKamer()
+            {
+                Console.Clear();
+                Console.WriteLine(Naam);
+                Console.WriteLine(Beschrijving);
+
+                SpeelPuzzel();
+            }
+
+            protected abstract void SpeelPuzzel();
+        }
+    }
+}
